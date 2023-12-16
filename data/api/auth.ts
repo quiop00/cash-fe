82663f53b12b5870b3e68fed20b18e25b29dfb86
Hoang Duc <mhoangduc25@gmail.com>
2023-12-16 15:31:47 +0700

    refactor code

--- data/api/auth.ts
+++ data/api/auth.ts
@@ -1,8 +1,8 @@
 import { User } from "data/models/User";
-import { BASE_URL, getApi, postApi } from "./base";
+import { BE_BASE_URL, getApi, postApi } from "./base";
 
-const REGISTER_URL = `${BASE_URL}/auth/register`;
-const LOGIN_URL = `${BASE_URL}/auth/login`;
+const REGISTER_URL = `${BE_BASE_URL}/auth/register`;
+const LOGIN_URL = `${BE_BASE_URL}/auth/login`;
 
 export const login = async (user: User) =>{
     return await getApi(LOGIN_URL, user);
--- data/api/user.ts
+++ data/api/user.ts
@@ -1,6 +1,6 @@
-import { BASE_URL, getApi } from "./base"
+import { BE_BASE_URL, getApi } from "./base"
 
-const USER_URL = `${BASE_URL}/users`;
+const USER_URL = `${BE_BASE_URL}/users`;
 
 export const fetchUsers = async () => {
     return await getApi(USER_URL);
